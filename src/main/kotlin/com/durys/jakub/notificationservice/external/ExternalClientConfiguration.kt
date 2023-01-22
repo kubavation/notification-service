1c35a9afc18f99a1a50e24bf922e1b84a106aea4
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-22 13:42:04 +0100

    ExternalClientConfiguration - configuration of mail service

---
+++ src/main/kotlin/com/durys/jakub/notificationservice/external/ExternalClientConfiguration.kt
@@ -0,0 +1,19 @@
+package com.durys.jakub.notificationservice.external
+
+import com.durys.jakub.notificationservice.external.mail.MailServiceClient
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.cloud.client.loadbalancer.LoadBalanced
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
+
+@Configuration
+internal class ExternalClientConfiguration {
+
+    @Bean
+    @LoadBalanced
+    fun mailWebClient(): WebClient.Builder = WebClient.builder().baseUrl("http://MAIL-SERVICE")
+
+    @Bean
+    fun mailServiceClient(@Qualifier("mailWebClient") webClient: WebClient.Builder): MailServiceClient = MailServiceClient(webClient)
+}
