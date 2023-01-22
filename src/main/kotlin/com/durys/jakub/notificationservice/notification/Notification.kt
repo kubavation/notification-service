7ba7313dd9ab1793e779fa8b6ac81ccb8028708b
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-22 12:32:10 +0100

    additional params in Notification model

--- src/main/kotlin/com/durys/jakub/notificationservice/notification/Notification.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/notification/Notification.kt
@@ -1,4 +1,16 @@
 package com.durys.jakub.notificationservice.notification
 
 class Notification(val tenantId: TenantId, val subject: String, val content: String) {
+    var withEmail = false
+    var url = "";
+
+    fun withEmail(value: Boolean): Notification {
+        this.withEmail = value
+        return this
+    }
+
+    fun withUrl(value: String): Notification {
+        this.url = value
+        return this
+    }
 }
