07cc99718b126d13c054c46a4be69f6adecdc27a
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-22 14:18:11 +0100

    initial implementation of send method - mail Service

--- src/main/kotlin/com/durys/jakub/notificationservice/external/mail/MailServiceClient.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/external/mail/MailServiceClient.kt
@@ -1,8 +1,16 @@
 package com.durys.jakub.notificationservice.external.mail
 
+import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
 
 internal class MailServiceClient(private val webClient: WebClient) {
 
+    fun send(mail: Mail) {
+        this.webClient
+            .post()
+            .uri("/mails")
+            .body(BodyInserters.fromValue(mail))
+            .exchange().block()
+    }
 
 }
