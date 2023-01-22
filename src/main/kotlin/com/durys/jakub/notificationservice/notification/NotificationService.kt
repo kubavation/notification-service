9ff224f393a327a9c60f61aac07880bff79c9e48
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-22 19:43:17 +0100

    process method in notification service

--- src/main/kotlin/com/durys/jakub/notificationservice/notification/NotificationService.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/notification/NotificationService.kt
@@ -1,10 +1,19 @@
 package com.durys.jakub.notificationservice.notification
 
 import com.durys.jakub.notificationservice.external.accessmanagement.AccessManagementServiceClient
+import com.durys.jakub.notificationservice.external.mail.Mail
 import com.durys.jakub.notificationservice.external.mail.MailServiceClient
 import org.springframework.stereotype.Service
 
 @Service
 internal class NotificationService(val mailServiceClient: MailServiceClient,
                                    val accessManagementServiceClient: AccessManagementServiceClient) {
+
+
+    fun process(notification: Notification) {
+        accessManagementServiceClient.receiverEmail(notification.tenantId)
+                .map { Mail(notification.subject, notification.content, it) }
+                .map { mailServiceClient.send(it) }
+                .block()
+    }
 }
