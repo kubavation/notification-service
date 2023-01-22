566691fad18cfa91639435a127d872655285eead
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-22 14:28:46 +0100

    initial AccessManagementServiceClient

---
+++ src/main/kotlin/com/durys/jakub/notificationservice/external/accessmanagement/AccessManagementServiceClient.kt
@@ -0,0 +1,6 @@
+package com.durys.jakub.notificationservice.external.accessmanagement
+
+import org.springframework.web.reactive.function.client.WebClient
+
+internal class AccessManagementServiceClient(private val webClient: WebClient) {
+}
