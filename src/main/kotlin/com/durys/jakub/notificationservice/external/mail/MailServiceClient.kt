dae7e17fc8955166bf5d35ec616270111083aeae
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-22 20:24:26 +0100

    change in send method

--- src/main/kotlin/com/durys/jakub/notificationservice/external/mail/MailServiceClient.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/external/mail/MailServiceClient.kt
@@ -1,16 +1,18 @@
 package com.durys.jakub.notificationservice.external.mail
 
 import org.springframework.web.reactive.function.BodyInserters
+import org.springframework.web.reactive.function.client.ClientResponse
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
 
 internal class MailServiceClient(private val webClient: WebClient) {
 
-    fun send(mail: Mail) {
-        webClient
+    fun send(mail: Mail): Mono<ClientResponse> {
+       return webClient
             .post()
             .uri("/mails")
             .body(BodyInserters.fromValue(mail))
-            .exchange().block()
+            .exchange()
     }
 
 }
