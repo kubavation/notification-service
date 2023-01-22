70f0b6c2ebc4d69ac18c756ec4d2f6e15d9078a6
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-22 15:07:18 +0100

    implementation of receiverEmail

--- src/main/kotlin/com/durys/jakub/notificationservice/external/accessmanagement/AccessManagementServiceClient.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/external/accessmanagement/AccessManagementServiceClient.kt
@@ -1,6 +1,18 @@
 package com.durys.jakub.notificationservice.external.accessmanagement
 
+import com.durys.jakub.notificationservice.notification.TenantId
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
 
 internal class AccessManagementServiceClient(private val webClient: WebClient) {
+
+    fun receiverEmail(tenantId: TenantId): Mono<String> {
+        return webClient
+                .get()
+                .uri("/users/%s/info".format(tenantId.value))
+                .retrieve()
+                .bodyToMono(Receiver::class.java)
+                .map { it.email }
+    }
+
 }
