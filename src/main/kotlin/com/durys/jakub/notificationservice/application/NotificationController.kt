1b2202644b0af157fe02452fd29bcacdc41b19d8
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-23 18:54:58 +0100

    websocket controller

--- src/main/kotlin/com/durys/jakub/notificationservice/application/NotificationController.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/application/NotificationController.kt
@@ -1,15 +1,17 @@
 package com.durys.jakub.notificationservice.application
 
 import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.messaging.handler.annotation.SendTo
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.stereotype.Controller
 
-@RestController
+@Controller
 class NotificationController {
 
     @MessageMapping("/notify")
-    @SendTo("/notifications/") //todo
-    fun send() {
-
+    @SendTo("/topic")
+    fun send(@Payload message: String): String {
+        println(message)
+        return message
     }
 }
