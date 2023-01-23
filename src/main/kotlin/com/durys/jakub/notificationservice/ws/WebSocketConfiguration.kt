a820fef58c24d369d4daf6440687355a1fef49af
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-23 17:24:36 +0100

    removed unused annotation

--- src/main/kotlin/com/durys/jakub/notificationservice/ws/WebSocketConfiguration.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/ws/WebSocketConfiguration.kt
@@ -8,7 +8,6 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 
 @Configuration
-@EnableScheduling
 @EnableWebSocketMessageBroker
 internal class WebSocketConfiguration: WebSocketMessageBrokerConfigurer {
 
