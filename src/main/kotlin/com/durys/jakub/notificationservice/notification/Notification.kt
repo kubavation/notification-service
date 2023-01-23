c7c50af7c26bed1ee3e12174a1671abf7313cf91
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-23 21:05:20 +0100

    type field in notification

--- src/main/kotlin/com/durys/jakub/notificationservice/notification/Notification.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/notification/Notification.kt
@@ -3,6 +3,7 @@ package com.durys.jakub.notificationservice.notification
 class Notification(val tenantId: TenantId? = null, val subject: String? = null, val content: String? = null) {
     var withEmail = false
     var url = "";
+    var type: List<NotificationType> = listOf(NotificationType.APP)
 
     fun withEmail(value: Boolean): Notification {
         this.withEmail = value
