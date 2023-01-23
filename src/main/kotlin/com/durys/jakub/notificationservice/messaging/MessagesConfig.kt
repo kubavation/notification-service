ee72cd7605569dc3658afed5e133507d12606636
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-23 20:48:40 +0100

    Jackson2JsonMessageConverter

--- src/main/kotlin/com/durys/jakub/notificationservice/messaging/MessagesConfig.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/messaging/MessagesConfig.kt
@@ -1,6 +1,7 @@
 package com.durys.jakub.notificationservice.messaging
 
 import org.springframework.amqp.core.Queue
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -9,4 +10,7 @@ internal class MessagesConfig {
 
     @Bean
     fun notificationQueue() = Queue("q.notification-queue")
+
+    @Bean
+    fun converter() = Jackson2JsonMessageConverter()
 }
