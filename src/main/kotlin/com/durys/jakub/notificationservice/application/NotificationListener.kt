9e837151a8c091a6944c3e6996cb9b4d935c79e3
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-23 20:54:09 +0100

    sending email based on value

--- src/main/kotlin/com/durys/jakub/notificationservice/application/NotificationListener.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/application/NotificationListener.kt
@@ -1,16 +1,23 @@
 package com.durys.jakub.notificationservice.application
 
 import com.durys.jakub.notificationservice.notification.Notification
+import com.durys.jakub.notificationservice.notification.NotificationService
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Service
 
 @Service
-internal class NotificationListener(private val messagingTemplate: SimpMessagingTemplate) {
+internal class NotificationListener(
+        private val messagingTemplate: SimpMessagingTemplate,
+        private val notificationService: NotificationService) {
 
     @RabbitListener(queues = ["q.notification-queue"])
     fun listen(notification: Notification) {
 
         messagingTemplate.convertAndSend("/topic/notifications", notification)
+
+        if (notification.withEmail) {
+            notificationService.process(notification)
+        }
     }
 }
