a6b781a4b38f94cc7031a414da20ef877e6828b3
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-24 20:09:43 +0100

    moved notification repository to infrastructure package

--- src/main/kotlin/com/durys/jakub/notificationservice/infrastructure/NotificationRepository.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/infrastructure/NotificationRepository.kt
@@ -1,5 +1,6 @@
-package com.durys.jakub.notificationservice.infrastructure.out
+package com.durys.jakub.notificationservice.infrastructure
 
+import com.durys.jakub.notificationservice.infrastructure.out.Notification
 import org.springframework.data.mongodb.repository.MongoRepository
 
 interface NotificationRepository: MongoRepository<Notification, String> {
