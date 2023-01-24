ac630bd6f47473f93e6b313fd16251dd744bfd27
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-24 20:02:16 +0100

    notification document

--- src/main/kotlin/com/durys/jakub/notificationservice/infrastructure/out/Notification.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/infrastructure/out/Notification.kt
@@ -1,4 +1,12 @@
 package com.durys.jakub.notificationservice.infrastructure.out
 
-class Notification {
+import com.durys.jakub.notificationservice.notification.NotificationType
+import com.durys.jakub.notificationservice.notification.TenantId
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document
+class Notification(val tenantId: TenantId,
+                   val subject: String,
+                   val content: String,
+                   val type: List<NotificationType>) {
 }
