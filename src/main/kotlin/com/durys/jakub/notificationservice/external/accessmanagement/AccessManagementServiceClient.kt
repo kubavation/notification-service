8294dc8a25b69a2b80dfe26c01c347a8fbed60a1
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-24 17:57:20 +0100

    TenantEmailNotFoundException

--- src/main/kotlin/com/durys/jakub/notificationservice/external/accessmanagement/AccessManagementServiceClient.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/external/accessmanagement/AccessManagementServiceClient.kt
@@ -1,8 +1,11 @@
 package com.durys.jakub.notificationservice.external.accessmanagement
 
 import com.durys.jakub.notificationservice.notification.TenantId
+
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
+
+class TenantEmailNotFoundException(tenantId: TenantId?): RuntimeException("Tenant email of id ${tenantId?.value} not found")
 
 internal class AccessManagementServiceClient(private val webClient: WebClient) {
 
@@ -12,6 +15,7 @@ internal class AccessManagementServiceClient(private val webClient: WebClient) {
                 .uri("/users/%s/info".format(tenantId?.value))
                 .retrieve()
                 .bodyToMono(Receiver::class.java)
+                .onErrorMap { TenantEmailNotFoundException(tenantId) }
                 .map { it.email }
     }
 
