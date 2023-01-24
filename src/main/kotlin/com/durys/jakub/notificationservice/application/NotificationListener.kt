b2ca6dd1b942bc078e11b1ed191e70ebf2e57c15
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-24 16:55:38 +0100

    logger in NotificationListener

--- src/main/kotlin/com/durys/jakub/notificationservice/application/NotificationListener.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/application/NotificationListener.kt
@@ -2,9 +2,12 @@ package com.durys.jakub.notificationservice.application
 
 import com.durys.jakub.notificationservice.notification.Notification
 import com.durys.jakub.notificationservice.notification.NotificationService
+import mu.KotlinLogging
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Service
+
+private val logger = KotlinLogging.logger {}
 
 @Service
 internal class NotificationListener(
@@ -13,6 +16,10 @@ internal class NotificationListener(
 
     @RabbitListener(queues = ["q.notification-queue"])
     fun listen(notification: Notification) {
+
+        logger.info {
+            "got notification for tenantId: ${notification.tenantId?.value} (with email: ${notification.withEmail}"
+        }
 
         messagingTemplate.convertAndSend("/topic/notifications", notification)
 
