4e21ac5a38a540cdbb51469dab4db066af16a292
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-25 19:44:47 +0100

    endpoints for deleting/deactivating

--- src/main/kotlin/com/durys/jakub/notificationservice/infrastructure/in/NotificationController.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/infrastructure/in/NotificationController.kt
@@ -3,8 +3,11 @@ package com.durys.jakub.notificationservice.infrastructure.`in`
 import com.durys.jakub.notificationservice.infrastructure.out.NotificationStatus
 import com.durys.jakub.notificationservice.notification.NotificationDTO
 import com.durys.jakub.notificationservice.notification.NotificationService
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -22,4 +25,9 @@ internal class NotificationController(val notificationService: NotificationServi
                 .orElseGet {  notificationService.findAllByTenantId(tenantId) }
     }
 
+    @PatchMapping("/deactivated")
+    fun deactivate(@RequestBody notifications: List<NotificationDTO>) = notificationService.markAsInactive(notifications)
+
+    @PatchMapping("/deleted")
+    fun delete(@RequestBody notifications: List<NotificationDTO>) = notificationService.markAsDeleted(notifications)
 }
