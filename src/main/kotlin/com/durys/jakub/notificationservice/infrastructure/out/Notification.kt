44c82ee1106a8e0e8f62136917482b5ee9b2579f
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-25 18:20:31 +0100

    creationDate field in notification

--- src/main/kotlin/com/durys/jakub/notificationservice/infrastructure/out/Notification.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/infrastructure/out/Notification.kt
@@ -3,6 +3,7 @@ package com.durys.jakub.notificationservice.infrastructure.out
 import com.durys.jakub.notificationservice.notification.NotificationType
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
 
 @Document
 class Notification(@Id val id: String,
@@ -12,5 +13,6 @@ class Notification(@Id val id: String,
                    val types: List<NotificationType>) {
 
     var status: NotificationStatus = NotificationStatus.ACTIVE
+    val creationDate: LocalDateTime = LocalDateTime.now()
 
 }
