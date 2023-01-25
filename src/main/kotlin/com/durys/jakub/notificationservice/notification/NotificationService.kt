a6cd6a5a1a3ff19a808dfa7b15b8a153820c5467
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-25 19:24:00 +0100

    find method in notification service

--- src/main/kotlin/com/durys/jakub/notificationservice/notification/NotificationService.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/notification/NotificationService.kt
@@ -1,12 +1,17 @@
 package com.durys.jakub.notificationservice.notification
 
+import com.durys.jakub.notificationservice.commons.NotificationAssembler
 import com.durys.jakub.notificationservice.external.accessmanagement.AccessManagementServiceClient
 import com.durys.jakub.notificationservice.external.mail.Mail
 import com.durys.jakub.notificationservice.external.mail.MailServiceClient
+import com.durys.jakub.notificationservice.infrastructure.NotificationRepository
+import com.durys.jakub.notificationservice.infrastructure.out.NotificationStatus
+import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
 
 @Service
 internal class NotificationService(val mailServiceClient: MailServiceClient,
+                                   val notificationRepository: NotificationRepository,
                                    val accessManagementServiceClient: AccessManagementServiceClient) {
 
 
@@ -16,5 +21,15 @@ internal class NotificationService(val mailServiceClient: MailServiceClient,
                 .flatMap { mailServiceClient.send(it) }
                 .onErrorComplete()
                 .block()
+    }
+
+    fun findAllByTenantIdAndStatus(tenantId: String, status: NotificationStatus): List<NotificationDTO> {
+        return notificationRepository.findAllByTenantAndStatus(tenantId, status, Sort.by(Sort.Direction.DESC,"creationDate"))
+                .map { NotificationAssembler.toDTO(it) }
+    }
+
+    fun findAllByTenantId(tenantId: String): List<NotificationDTO> {
+        return notificationRepository.findAllByTenant(tenantId, Sort.by(Sort.Direction.DESC,"creationDate"))
+                .map { NotificationAssembler.toDTO(it) }
     }
 }
