88783b17e91f9be11b74b94b4bda9e6f08b82651
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-25 17:14:40 +0100

    NotificationAssembler

---
+++ src/main/kotlin/com/durys/jakub/notificationservice/commons/NotificationAssembler.kt
@@ -0,0 +1,12 @@
+package com.durys.jakub.notificationservice.commons
+
+import com.durys.jakub.notificationservice.infrastructure.out.Notification
+import com.durys.jakub.notificationservice.notification.NotificationDTO
+import java.util.UUID
+
+object NotificationAssembler {
+
+    fun toEntity(dto: NotificationDTO): Notification {
+        return Notification(UUID.randomUUID().toString(), dto.tenantId!!, dto.subject!!, dto.content!!, dto.types)
+    }
+}
