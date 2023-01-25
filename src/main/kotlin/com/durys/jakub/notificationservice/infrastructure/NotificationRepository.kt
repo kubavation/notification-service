45eb570cf06df88f4031256153187c63085a6bbd
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-25 18:38:07 +0100

    notification repository find methods

--- src/main/kotlin/com/durys/jakub/notificationservice/infrastructure/NotificationRepository.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/infrastructure/NotificationRepository.kt
@@ -1,7 +1,16 @@
 package com.durys.jakub.notificationservice.infrastructure
 
 import com.durys.jakub.notificationservice.infrastructure.out.Notification
+import com.durys.jakub.notificationservice.infrastructure.out.NotificationStatus
+import org.springframework.data.domain.Sort
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
 
 interface NotificationRepository: MongoRepository<Notification, String> {
+
+    @Query("{'tenantId' : ?0}")
+    fun findAllByTenant(tenantId: String, sort: Sort)
+
+    @Query("{'tenantId' : ?0, 'status' : ?1}")
+    fun findAllByTenantAndStatus(tenantId: String, status: NotificationStatus, sort: Sort)
 }
