384e2ec14d18b9e4ac61b1bc50b82287a37028d4
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-25 18:16:56 +0100

    endpoint for finding notification based on status

--- src/main/kotlin/com/durys/jakub/notificationservice/infrastructure/in/NotificationController.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/infrastructure/in/NotificationController.kt
@@ -1,10 +1,25 @@
 package com.durys.jakub.notificationservice.infrastructure.`in`
 
+import com.durys.jakub.notificationservice.infrastructure.out.NotificationStatus
+import com.durys.jakub.notificationservice.notification.NotificationDTO
 import com.durys.jakub.notificationservice.notification.NotificationService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
-@RequestMapping("/notifications")
+@RequestMapping("/tenants/{tenantId}/notifications")
 internal class NotificationController(val notificationService: NotificationService) {
+
+    @GetMapping
+    fun findAllByStatus(@PathVariable tenantId: String,
+                        @RequestParam status: Optional<NotificationStatus>): List<NotificationDTO> {
+        return status
+                .map { notificationService.findAllByTenantIdAndStatus(tenantId, it) }
+                .orElseGet {  notificationService.findAllByTenantId(tenantId) }
+    }
+
 }
