3d2500a4aecf5cb5b1fe56d4a0abd73f462092a3
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-26 17:48:18 +0100

    init of SmSNotificationDeliveryStrategy

---
+++ src/main/kotlin/com/durys/jakub/notificationservice/notification/deliverystrategies/SmSNotificationDeliveryStrategy.kt
@@ -0,0 +1,10 @@
+package com.durys.jakub.notificationservice.notification.deliverystrategies
+
+import com.durys.jakub.notificationservice.infrastructure.out.Notification
+
+internal class SmSNotificationDeliveryStrategy(): NotificationDeliveryStrategy {
+
+    override fun deliver(notification: Notification) {
+        TODO()
+    }
+}
