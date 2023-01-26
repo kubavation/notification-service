7d46471d63e7a4d6a31ada20582ef22c4b4b8c4b
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-26 17:47:35 +0100

    MailNotificationDeliveryStrategy

---
+++ src/main/kotlin/com/durys/jakub/notificationservice/notification/deliverystrategies/MailNotificationDeliveryStrategy.kt
@@ -0,0 +1,20 @@
+package com.durys.jakub.notificationservice.notification.deliverystrategies
+
+import com.durys.jakub.notificationservice.external.accessmanagement.AccessManagementServiceClient
+import com.durys.jakub.notificationservice.external.mail.Mail
+import com.durys.jakub.notificationservice.external.mail.MailServiceClient
+import com.durys.jakub.notificationservice.infrastructure.out.Notification
+import com.durys.jakub.notificationservice.notification.TenantId
+
+
+internal class MailNotificationDeliveryStrategy(private val mailServiceClient: MailServiceClient,
+                                                private val accessManagementServiceClient: AccessManagementServiceClient): NotificationDeliveryStrategy {
+
+    override fun deliver(notification: Notification) {
+        accessManagementServiceClient.receiverEmail(TenantId(notification.tenantId))
+                .map { Mail(notification.subject, notification.content, it) }
+                .flatMap { mailServiceClient.send(it) }
+                .onErrorComplete()
+                .block()
+    }
+}
