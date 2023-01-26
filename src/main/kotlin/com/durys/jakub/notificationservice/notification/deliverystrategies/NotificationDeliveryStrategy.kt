2a6643935342f4f5f6f109a1fe5f58cd480bc3a1
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-26 17:40:44 +0100

    changes in visibility

--- src/main/kotlin/com/durys/jakub/notificationservice/notification/deliverystrategies/NotificationDeliveryStrategy.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/notification/deliverystrategies/NotificationDeliveryStrategy.kt
@@ -2,6 +2,6 @@ package com.durys.jakub.notificationservice.notification.deliverystrategies
 
 import com.durys.jakub.notificationservice.infrastructure.out.Notification
 
-interface NotificationDeliveryStrategy {
+internal interface NotificationDeliveryStrategy {
     fun deliver(notification: Notification)
 }
--- src/main/kotlin/com/durys/jakub/notificationservice/notification/deliverystrategies/WebSocketNotificationDeliveryStrategy.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/notification/deliverystrategies/WebSocketNotificationDeliveryStrategy.kt
@@ -3,7 +3,7 @@ package com.durys.jakub.notificationservice.notification.deliverystrategies
 import com.durys.jakub.notificationservice.infrastructure.out.Notification
 import org.springframework.messaging.simp.SimpMessagingTemplate
 
-class WebSocketNotificationDeliveryStrategy(private val messagingTemplate: SimpMessagingTemplate): NotificationDeliveryStrategy {
+internal class WebSocketNotificationDeliveryStrategy(private val messagingTemplate: SimpMessagingTemplate): NotificationDeliveryStrategy {
 
     override fun deliver(notification: Notification) {
         messagingTemplate
