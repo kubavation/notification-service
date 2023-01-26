2384a7ec90561401a090bba31be054ed22c35501
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-26 18:25:13 +0100

    configuration of ws.notification.destination

--- src/main/kotlin/com/durys/jakub/notificationservice/notification/deliverystrategies/WebSocketNotificationDeliveryStrategy.kt
+++ src/main/kotlin/com/durys/jakub/notificationservice/notification/deliverystrategies/WebSocketNotificationDeliveryStrategy.kt
@@ -1,14 +1,16 @@
 package com.durys.jakub.notificationservice.notification.deliverystrategies
 
 import com.durys.jakub.notificationservice.infrastructure.out.Notification
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Service
 
 @Service
-internal class WebSocketNotificationDeliveryStrategy(private val messagingTemplate: SimpMessagingTemplate): NotificationDeliveryStrategy {
+internal class WebSocketNotificationDeliveryStrategy(private val messagingTemplate: SimpMessagingTemplate,
+                                                     @Value("\${ws.notification.destination}") private val destination: String): NotificationDeliveryStrategy {
 
     override fun deliver(notification: Notification) {
         messagingTemplate
-                .convertAndSendToUser(notification.tenantId, "/queue/notifications", notification)
+                .convertAndSendToUser(notification.tenantId, destination, notification)
     }
 }
