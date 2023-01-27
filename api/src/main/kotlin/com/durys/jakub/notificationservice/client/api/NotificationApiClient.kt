4f858065b0c19ee299dafde830ee0f325b2a5422
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-27 20:30:35 +0100

    NotificationApiClient

--- api/src/main/kotlin/com/durys/jakub/notificationservice/client/api/NotificationApiClient.kt
+++ api/src/main/kotlin/com/durys/jakub/notificationservice/client/api/NotificationApiClient.kt
@@ -1,4 +1,15 @@
 package com.durys.jakub.notificationservice.client.api
 
-class NotificationApiClient {
+import com.durys.jakub.notificationservice.notification.NotificationDTO
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+
+@Service
+class NotificationApiClient(private val rabbitTemplate: RabbitTemplate,
+                            @Value("\${queue.notification}") private val queue: String) {
+
+
+    fun publish(notification: NotificationDTO)
+            = rabbitTemplate.convertAndSend(queue, notification)
 }
