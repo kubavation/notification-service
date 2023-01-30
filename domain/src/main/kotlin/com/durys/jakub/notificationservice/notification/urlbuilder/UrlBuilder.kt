d88738190e604233790908d44dac5de21f2c0584
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-30 18:09:34 +0100

    urlBuilder -> build method

--- domain/src/main/kotlin/com/durys/jakub/notificationservice/notification/urlbuilder/UrlBuilder.kt
+++ domain/src/main/kotlin/com/durys/jakub/notificationservice/notification/urlbuilder/UrlBuilder.kt
@@ -1,8 +1,17 @@
 package com.durys.jakub.notificationservice.notification.urlbuilder
 
+
+fun String.href(ref: String) = "$this href=\"${ref}\">"
+
+fun String.label(label: String) ="$this>${label}"
+
+fun String.build() = "$this</a>"
+
 object UrlBuilder {
 
+    private const val Link = "<a";
+
     fun build(url: String): String {
-        return url
+        return Link.href(url).label("Link to application").build()
     }
 }
