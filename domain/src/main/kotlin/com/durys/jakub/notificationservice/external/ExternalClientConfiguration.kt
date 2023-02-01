9cd14ac12423739ed262721aa08c20f5be480548
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-01 19:58:50 +0100

    services url in bean config

--- domain/src/main/kotlin/com/durys/jakub/notificationservice/external/ExternalClientConfiguration.kt
+++ domain/src/main/kotlin/com/durys/jakub/notificationservice/external/ExternalClientConfiguration.kt
@@ -3,6 +3,7 @@ package com.durys.jakub.notificationservice.external
 import com.durys.jakub.notificationservice.external.accessmanagement.AccessManagementServiceClient
 import com.durys.jakub.notificationservice.external.mail.MailServiceClient
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -13,14 +14,16 @@ internal class ExternalClientConfiguration {
 
     @Bean
     @LoadBalanced
-    fun mailWebClient(): WebClient.Builder = WebClient.builder().baseUrl("http://MAIL-SERVICE")
+    fun mailWebClient(@Value("\${mail.service.url}") mailServiceUrl: String): WebClient.Builder =
+            WebClient.builder().baseUrl(mailServiceUrl)
 
     @Bean
     fun mailServiceClient(@Qualifier("mailWebClient") webClient: WebClient.Builder): MailServiceClient = MailServiceClient(webClient.build())
 
     @Bean
     @LoadBalanced
-    fun accessManagementWebClient(): WebClient.Builder = WebClient.builder().baseUrl("http://ACCESS-MANAGEMENT")
+    fun accessManagementWebClient(@Value("\${access-management.service.url}") amServiceUrl: String): WebClient.Builder =
+            WebClient.builder().baseUrl(amServiceUrl)
 
     @Bean
     fun accessManagementServiceClient(@Qualifier("accessManagementWebClient") webClient: WebClient.Builder): AccessManagementServiceClient
