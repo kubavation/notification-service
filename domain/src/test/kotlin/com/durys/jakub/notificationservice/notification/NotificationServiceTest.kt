7b31ae229d07719978dc99294ea54ccc94b4d4b3
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-01 19:22:20 +0100

    tests for deleting notifications

--- domain/src/test/kotlin/com/durys/jakub/notificationservice/notification/NotificationServiceTest.kt
+++ domain/src/test/kotlin/com/durys/jakub/notificationservice/notification/NotificationServiceTest.kt
@@ -36,4 +36,27 @@ internal class NotificationServiceTest {
             assertEquals(NotificationStatus.INACTIVE, it.status)
         }
     }
+
+
+    @Test
+    fun markNotificationsAsDeleted_shouldChangeStatusToDeleted() {
+        val notifications = listOf(
+                NotificationDTO(TenantId("1"), "test1", "test1", listOf(NotificationType.APP))
+                        .withId("1"),
+                NotificationDTO(TenantId("2"), "test2", "test2", listOf(NotificationType.APP))
+                        .withId("2")
+        )
+
+        val entities = listOf(
+                Notification("1", "1", "test1", "test1", null, listOf(NotificationType.APP)),
+                Notification("2", "1", "test2", "test2", null, listOf(NotificationType.APP)))
+
+        Mockito.`when`(notificationRepository.findAllById(listOf("1", "2"))).thenReturn(entities)
+
+        notificationService.markAsDeleted(notifications)
+
+        entities.forEach {
+            assertEquals(NotificationStatus.DELETED, it.status)
+        }
+    }
 }
