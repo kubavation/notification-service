bd6a990c1354f0632a190181a2e17ce7e2cb6fcd
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-01 18:44:16 +0100

    simplification of urlBuilder

--- domain/src/main/kotlin/com/durys/jakub/notificationservice/notification/urlbuilder/UrlBuilder.kt
+++ domain/src/main/kotlin/com/durys/jakub/notificationservice/notification/urlbuilder/UrlBuilder.kt
@@ -1,15 +1,6 @@
 package com.durys.jakub.notificationservice.notification.urlbuilder
 
-
-fun String.href(ref: String) = "$this href=\"${ref}\">"
-
-fun String.label(label: String) ="$this>${label}"
-
-fun String.build() = "$this</a>"
-
 object UrlBuilder {
 
-    private const val Link = "<a";
-
-    fun build(url: String) = Link.href(url).label("Link to application").build()
+    fun build(url: String) = "<a href=\"${url}\">Link to application</a>"
 }
