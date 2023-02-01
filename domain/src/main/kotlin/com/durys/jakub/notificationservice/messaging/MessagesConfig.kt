7b9a769301cc4de5dc51787a767becbd26bae538
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-01 20:01:54 +0100

    MessagesConfig property

--- domain/src/main/kotlin/com/durys/jakub/notificationservice/messaging/MessagesConfig.kt
+++ domain/src/main/kotlin/com/durys/jakub/notificationservice/messaging/MessagesConfig.kt
@@ -2,6 +2,7 @@ package com.durys.jakub.notificationservice.messaging
 
 import org.springframework.amqp.core.Queue
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -9,7 +10,8 @@ import org.springframework.context.annotation.Configuration
 internal class MessagesConfig {
 
     @Bean
-    fun notificationQueue() = Queue("q.notification-queue")
+    fun notificationQueue(@Value("\${queue.notification}") notificationQueue: String)
+            = Queue(notificationQueue)
 
     @Bean
     fun converter() = Jackson2JsonMessageConverter()
